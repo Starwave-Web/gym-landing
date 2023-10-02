de791a7708379e0dab8c4e54091bb98f2ec60b6b
eudvardi2 <udvardiendre@gmail.com>
2023-10-02 22:42:48 +0200

    First

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { BenefitType, SelectedPage } from '@/shared/types'
 import {HomeModernIcon, UserGroupIcon, AcademicCapIcon} from "@heroicons/react/24/solid"
 import { motion } from 'framer-motion'
